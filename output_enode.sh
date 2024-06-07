ae1dcc6d1d405eb18285e28b891a6be5ef13d65a
Jeremy Crane <jeremyccrane@me.com>
2024-06-06 21:04:29 -0600

    Update output_enode.sh

--- output_enode.sh
+++ output_enode.sh
@@ -14,4 +14,4 @@ geth attach http://localhost:8545 --exec admin.nodeInfo.enode
 kill $GETH_PID
 
 # Start Geth normally
-exec geth --datadir /data --syncmode "fast" --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3 --networkid 110110 --mine --miner.etherbase=0xYourCoinbaseAddress --bootnodes "$BOOTNODES"
+exec geth --datadir /data --syncmode "full" --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3 --networkid 110110 --mine --miner.etherbase=0xYourCoinbaseAddress --bootnodes "$BOOTNODES"
