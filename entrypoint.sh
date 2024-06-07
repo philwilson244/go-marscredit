90940576753d77dfc963531e0929c01c7f9517de
Jeremy Crane <jeremyccrane@me.com>
2024-06-06 20:44:20 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@
 geth init /genesis.json
 
 # Start Geth and enable mining
-#exec geth --datadir /data --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3 --networkid 110110 --mine --miner.etherbase=0xD21602919e81e32A456195e9cE34215Af504535A --bootnodes "enode://<node1_id>@node1.marscredit.xyz:30303,enode://<node2_id>@node2.marscredit.xyz:30303,enode://<node3_id>@node3.marscredit.xyz:30303"
+#exec geth --datadir /data --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3 --networkid 110110 --mine --miner.etherbase=0xD21602919e81e32A456195e9cE34215Af504535A --bootnodes "$BOOTNODES"
 exec geth --datadir /data --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3 --networkid 110110 --mine --miner.etherbase=0xD21602919e81e32A456195e9cE34215Af504535A
