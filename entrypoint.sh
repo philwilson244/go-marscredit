81e59f9b4fa6f98bd80094c0d028e19244ca0d1c
Jeremy Crane <jeremyccrane@me.com>
2024-06-06 20:29:44 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 geth init /genesis.json
 
 # Start Geth and enable mining
-exec geth --datadir /data --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3 --networkid 110110 --mine --miner.threads=1 --miner.etherbase=0xD21602919e81e32A456195e9cE34215Af504535A
+exec geth --datadir /data --http --http.addr 0.0.0.0 --http.port 8545 --http.api personal,eth,net,web3 --networkid 110110 --mine --miner.etherbase=0xD21602919e81e32A456195e9cE34215Af504535A
