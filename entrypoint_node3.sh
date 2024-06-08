3bcef52b9f93a6f245d08ad5fb32351e62156c18
Jeremy Crane <jeremyccrane@me.com>
2024-06-08 13:20:43 -0600

    updates

--- entrypoint_node3.sh
+++ entrypoint_node3.sh
@@ -6,4 +6,4 @@ echo "Starting Node 3"
 geth init /genesis.json
 
 # Start Geth and enable mining
-exec geth --datadir /data --syncmode "full" --http --http.addr 0.0.0.0 --http.port 8543 --http.api personal,eth,net,web3 --http.vhosts=* --http.corsdomain=* --networkid 110110 --ws --ws.addr "0.0.0.0" --ws.port 8546 --mine --miner.etherbase=0xD21602919e81e32A456195e9cE34215Af504535A --bootnodes "$BOOTNODES"  --allow-insecure-unlock --verbosity 5 --maxpeers 50 --cache 2048
+exec geth --datadir /data --syncmode "full" --http --http.addr 0.0.0.0 --http.port 8543 --http.api personal,eth,net,web3 --http.vhosts=* --http.corsdomain=* --networkid 110110 --ws --ws.addr "0.0.0.0" --ws.port 8546 --mine --miner.etherbase=0xD21602919e81e32A456195e9cE34215Af504535A --bootnodes "$BOOTNODES"  --allow-insecure-unlock --verbosity 5 --maxpeers 50 --cache 2048 --nodiscover
