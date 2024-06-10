b2c7e243961b2202ef66b8d82af2710b745daa23
Jeremy Crane <jeremyccrane@me.com>
2024-06-09 23:56:07 -0600

    Updates

--- entrypoint_node2.sh
+++ entrypoint_node2.sh
@@ -6,18 +6,16 @@ echo "Starting Node 2"
 exec geth --datadir /app/data \
     --syncmode "full" \
     --http \
-    --http.addr 0.0.0.0 \
     --http.port 8542 \
     --http.api personal,eth,net,web3,miner \
     --http.vhosts=* \
     --http.corsdomain=* \
     --networkid 110110 \
     --ws \
-    --ws.addr "0.0.0.0" \
     --ws.port 8545 \
     --mine \
     --miner.etherbase 0xD21602919e81e32A456195e9cE34215Af504535A \
-    --bootnodes "enode://41cb4f04ac043653e60319939ffe266d8d8470829f1a433a2d6b11839888d2b32bd0e66aba812d4ac93a271e4e7418f858fe06f7a32e0d7f3a0177527f91a6c0@node1.marscredit.xyz:8541?discport=0" \
+    --bootnodes "enode://7dd435aef0596d6cc6d5b51144211cbbb5b8c99240ab481f636ed73e2297ad96b5c6495be5bac7913fda12ad5caa5c6cea902a96422916ab29b7a9277b75c57f@node1.marscredit.xyz:443?discport=0" \
     --allow-insecure-unlock \
     --verbosity 6 \
     --maxpeers 50 \
