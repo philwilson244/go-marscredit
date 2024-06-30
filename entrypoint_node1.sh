ad8599f9e0d1bc8fff9fe5379afc5bca03f6f99b
Jeremy Crane <jeremyccrane@me.com>
2024-06-30 11:12:40 -0600

    - u

--- entrypoint_node1.sh
+++ entrypoint_node1.sh
@@ -72,14 +72,14 @@ geth --datadir /data \
     --syncmode "full" \
     --http \
     --http.addr "0.0.0.0" \
-    --http.port 30303 \
+    --http.port 8545 \
     --http.api personal,eth,net,web3,miner,admin \
     --http.vhosts=* \
     --http.corsdomain=* \
     --networkid 110110 \
     --ws \
     --ws.addr "0.0.0.0" \
-    --ws.port 30304 \
+    --ws.port 8546 \
     --mine \
     --miner.threads=1 \
     --miner.etherbase 0xc1133A2B8E92a747eBF2A937bE3D79c29231f407 \
