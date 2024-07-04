aa95696d0f755ffefc0fa69db7943e7736bef74b
Jeremy Crane <jeremyccrane@me.com>
2024-07-04 13:03:06 -0600

    Update entrypoint_node5.sh

--- entrypoint_node5.sh
+++ entrypoint_node5.sh
@@ -77,7 +77,7 @@ exec geth --datadir /data \
     --ws \
     --ws.addr 0.0.0.0 \
     --ws.port 8546 \
-    --port 30303 \
+    --port 30304 \
     --nat "any" \
     --mine \
     --miner.threads=1 \
