0d725ceacf500cf65fdf4c077f919f22cf9f66b5
Jeremy Crane <jeremyccrane@me.com>
2024-07-06 12:10:29 -0600

    - u

--- entrypoint_node99.sh
+++ entrypoint_node99.sh
@@ -77,7 +77,7 @@ exec geth --datadir /data \
     --ws \
     --ws.addr 0.0.0.0 \
     --ws.port 8546 \
-    --port 30304 \
+    --port 30305 \
     --nat "none" \
     --mine \
     --miner.threads=1 \
